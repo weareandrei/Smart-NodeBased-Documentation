643b142f9318e9d6c75a35045cb37e010c4ae5e4
weareandrei <weareandrei@gmail.com>
2024-02-18 15:53:23 +0000

    Merged + Fixed Eslint + TS configured for backend and frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,24 @@
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 
-import { validateUserCredentials } from './auth/auth.js';
-import { loadDocumentation } from './mongodb/get.js';
-import { syncNodes } from './mongodb/post.js';
+import { validateUserCredentials } from './auth/auth.js'
+import { loadDocumentation } from './mongodb/get.js'
+import { syncNodes } from './mongodb/post.js'
 
-import { getCurrentSprint, getCurrentSprintIssues } from './integration/taskmanager/jira/index.js';
+import { getCurrentSprint, getCurrentSprintIssues } from './integration/taskmanager/jira/index.js'
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-app.get('/taskmanager/jira/sprint/current', getCurrentSprint);
-app.get('/taskmanager/jira/sprint/current/issues', getCurrentSprintIssues);
+app.get('/taskmanager/jira/sprint/current', getCurrentSprint)
+app.get('/taskmanager/jira/sprint/current/issues', getCurrentSprintIssues)
 // app.get('/taskmanager/jira/sprint/{sprintId}/issues');
 
-app.post('/validateUserCredentials', validateUserCredentials);
-app.get('/loadDocumentation', loadDocumentation);
-app.post('/syncNodes', syncNodes);
+app.post('/validateUserCredentials', validateUserCredentials)
+app.get('/loadDocumentation', loadDocumentation)
+app.post('/syncNodes', syncNodes)
 
 app.listen(8081, () => {
-    console.log(`Server Started at ${8081}`);
-});
+    console.log(`Server Started at ${8081}`)
+})
